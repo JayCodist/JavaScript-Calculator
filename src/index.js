0fa7f3f8095d601ff3eb7110187679f412583775
Johnson Ubaezuonu <jaycodist@gmail.com>
2019-10-28 21:23:18 +0100

    implemented basic arithmetic operations -Johnson

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./index.css";
+import Calculator from './components/Calculator.js';
+import './styles/index.css';
 
 
+const App = props => (
+	<div>
+		<h1 className="center-text"> JavaScript Calculator </h1>
+		<Calculator />
+	</div>
+	)
 
-
-//ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
