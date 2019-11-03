335cd5d4e9b68e18fd73f7acfc580bd92fe0fc61
Johnson Ubaezuonu <jaycodist@gmail.com>
2019-11-03 23:24:25 +0100

    added handlers for key events -Johnson

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,7 +13,7 @@ const Calculator = props =>
 	const handleNumberInput = ({target}) =>
 	{
 		let newFormula;
-		if (target.value === '.')
+		if (target.value === '.') // Ensure no multiple decimal points
 		{
 			let lastNumIndex = Math.max(formula.lastIndexOf('/'), formula.lastIndexOf('*'), 
 				formula.lastIndexOf('+'), formula.lastIndexOf('-')) + 1;
@@ -24,12 +24,19 @@ const Calculator = props =>
 			else
 				newFormula = formula + target.value;
 		}
-		else if (isNewResult)
+		else if (isNewResult) // Start afresh after each calculation by resetting formula string
 		{
 			newFormula = target.value
 		}
 		else
 		{
+			if (target.value === "0") // No multiple leading zero
+			{
+				let lastNumIndex = Math.max(formula.lastIndexOf('/'), formula.lastIndexOf('*'), 
+				formula.lastIndexOf('+'), formula.lastIndexOf('-')) + 1;
+				if (formula.length - 1 === lastNumIndex && formula[formula.length - 1] === "0")
+					return;
+			}
 			newFormula = formula + target.value;
 		}
 		setFormula(newFormula);
@@ -39,14 +46,24 @@ const Calculator = props =>
 	const handleFuncInput = ({target}) =>
 	{
 		let newFormula;
-		if (isNewResult)
+		if (isNewResult) // Allow new result to be included in further calculations
 		{
 			newFormula = result.toString() + target.value;
 		}
+		else if (formula === "") // Operands must come first in a formula
+			return;
 		else
 		{
-			if (/\D$/.test(formula) || /\.$/.test(formula))
-				newFormula = formula.substring(0, formula.length - 1) + target.value;
+			// Disallow for consecutive operators except for - being added
+			if (/[/*\-+]$/.test(formula))
+			{
+				let lastOperandIndex = /(\d|\.)[^\d.]*$/.exec(formula).index;// "2+3+4.6+"
+				let lastOperatorSet = formula[lastOperandIndex + 1];
+				if (target.value === "-" && lastOperatorSet != "-")
+					newFormula = formula + target.value;
+				else
+					newFormula = formula.substring(0, lastOperandIndex + lastOperatorSet.length) + target.value;
+			}
 			else
 				newFormula = formula + target.value;
 		}
@@ -61,8 +78,10 @@ const Calculator = props =>
 	}
 	const performCalculation = str =>
 	{
+		if (!str.length)
+			return;
 		// eslint-disable-next-line
-		setResult(Function('"use strict";return (' + str + ')')());
+		setResult(Function('return (' + str + ')')());
 		setIsNewResult(true);
 	}
 
@@ -107,5 +126,41 @@ const Calculator = props =>
 			</section>
 		</div>)
 }
+
+
+document.addEventListener("keydown", e =>
+{
+	switch(e.code)
+	{
+		case "Escape": document.getElementById("clear").click(); break;
+		case "Enter": document.getElementById("equals").click(); break;
+		case "Period": document.getElementById("decimal").click(); break;
+		case "Slash": document.getElementById("divide").click(); break;
+		case "Minus": document.getElementById("subtract").click(); break;
+		case "Equal": 
+			if (e.shiftKey)
+				document.getElementById("add").click();
+			else
+				document.getElementById("equals").click(); break;
+		case "Digit8": 
+			if (e.shiftKey)
+				document.getElementById("multiply").click(); 
+			else
+				document.getElementById("eight").click(); break;
+				break;
+		case "KeyX": document.getElementById("multiply").click(); break;
+
+		case "Digit0": document.getElementById("zero").click(); break;
+		case "Digit1": document.getElementById("one").click(); break;
+		case "Digit2": document.getElementById("two").click(); break;
+		case "Digit3": document.getElementById("three").click(); break;
+		case "Digit4": document.getElementById("four").click(); break;
+		case "Digit5": document.getElementById("five").click(); break;
+		case "Digit6": document.getElementById("six").click(); break;
+		case "Digit7": document.getElementById("seven").click(); break;
+		case "Digit9": document.getElementById("nine").click(); break;
+	}
+
+})
 
 export default Calculator;
